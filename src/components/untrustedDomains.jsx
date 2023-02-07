904aefcd5740a4eb4222fc378f8f17edbf20a4ab
Omar Majzoub <omarmajzoub1@hotmail.com>
2023-02-07 16:32:48 +0200

    Worked on adding untrusted domains

--- src/components/untrustedDomains.jsx
+++ src/components/untrustedDomains.jsx
@@ -7,6 +7,8 @@ import { useState, useEffect } from "react";
 
 const UntrustedDomains = () => {
   const [untrustedDomains, setUntrustedDomains] = useState(null);
+  const [domainToAdd, setDomainToAdd] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     getUntrustedDomains(setUntrustedDomains);
@@ -19,6 +21,49 @@ const UntrustedDomains = () => {
         <h1>Untrusted Domains</h1>
       </div>
       <SearchBar placeholder_value="Email domain name" />
+      {localStorage.getItem("sc_list_owner").toString() ===
+      localStorage.getItem("sc_email") ? (
+        <div className="add-item">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Domain name"
+              aria-label="Domain name"
+              onChange={(e) => {
+                setDomainToAdd(e.target.value);
+              }}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+              onClick={() => {
+                addUntrustedDomain(
+                  domainToAdd,
+                  setErrorMessage,
+                  getUntrustedDomains,
+                  setUntrustedDomains
+                );
+              }}
+            >
+              Add
+            </button>
+          </div>
+          <div
+            id="errorMessage"
+            className={
+              "mb-4 text-" +
+              (errorMessage === "Successfully added" ? "success" : "danger")
+            }
+          >
+            {errorMessage}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+
       {untrustedDomains ? (
         <DomainList retrieved_list={untrustedDomains} />
       ) : (
@@ -30,7 +75,7 @@ const UntrustedDomains = () => {
   );
 };
 
-function getUntrustedDomains(setUntrustedDomains) {
+async function getUntrustedDomains(setUntrustedDomains) {
   var myHeaders = new Headers();
   myHeaders.append(
     "Authorization",
@@ -70,6 +115,70 @@ function getUntrustedDomains(setUntrustedDomains) {
       }
     })
     .catch((error) => console.log("error", error));
+}
+
+async function addUntrustedDomain(
+  domainToAdd,
+  setErrorMessage,
+  getUntrustedDomains,
+  setUntrustedDomains
+) {
+  let add_button = document.getElementById("errorMessage");
+  add_button.disabled = true;
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "Bearer " + localStorage.getItem("sc_acc_token")
+  );
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    domain: domainToAdd,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  await fetch("http://localhost:4000/untrusted_domains", requestOptions)
+    .then((response) => {
+      if (response.status === 500) {
+        console.clear();
+        return null;
+      } else if (response.status === 403) {
+        requestNewToken(addUntrustedDomain, [
+          domainToAdd,
+          setErrorMessage,
+          getUntrustedDomains,
+          setUntrustedDomains,
+        ]);
+      } else if (response.ok) {
+        return response;
+      } else if (response.status === 406) {
+        return response.json();
+      }
+      return null;
+    })
+    .then((result) => {
+      if (result) {
+        if (result.details) {
+          setErrorMessage(
+            result.details[0].message.replace(/"domain"/, "Domain")
+          );
+        } else if (result.message) {
+          setErrorMessage(result.message);
+        } else {
+          setErrorMessage("Successfully added");
+          getUntrustedDomains(setUntrustedDomains);
+        }
+      }
+    })
+    .catch((error) => console.log("error", error));
+
+  add_button.disabled = false;
 }
 
 export default UntrustedDomains;
