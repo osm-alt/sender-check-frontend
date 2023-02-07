3a70badb9aa0ef0b0cf225aea45fd5730c85ad3f
Omar Majzoub <omarmajzoub1@hotmail.com>
2023-02-07 16:20:54 +0200

    Worked on adding trusted domains

--- src/components/domainList.jsx
+++ src/components/domainList.jsx
@@ -1,24 +1,34 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-class DomainList extends Component {
-  state = {
-    trusted_domains: this.props.retrieved_list,
-  };
-  render() {
-    return (
-      <table className="table table-hover senders-table">
-        <thead>
-          <tr>
-            <th>Email domain name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.trusted_domains ? (
-            this.state.trusted_domains.map((domain) => {
-              return (
-                <tr key={domain + "_row"}>
-                  <td className="d-flex justify-content-between align-items-start">
-                    <span className="mt-1">{domain}</span>
+const DomainList = (props) => {
+  const [trustedDomains, setTrustedDomains] = useState(props.retrieved_list);
+
+  const is_owner =
+    localStorage.getItem("sc_list_owner") === localStorage.getItem("sc_email");
+
+  useEffect(() => {
+    setTrustedDomains(props.retrieved_list);
+    return;
+  }, [props.retrieved_list]);
+
+  return (
+    <table
+      className={"table table-hover senders-table" + (is_owner ? "-owner" : "")}
+    >
+      <thead>
+        <tr>
+          <th>Email domain name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {trustedDomains ? (
+          trustedDomains.map((domain) => {
+            return (
+              <tr key={domain + "_row"}>
+                <td className="d-flex justify-content-between align-items-start">
+                  <span className="mt-1">{domain}</span>
+                  {is_owner ? (
                     <button className="ms-5 btn btn-danger">
                       <i
                         className="bi pe-none bi-trash-fill"
@@ -26,17 +36,19 @@ class DomainList extends Component {
                         height="16"
                       ></i>
                     </button>
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <></>
-          )}
-        </tbody>
-      </table>
-    );
-  }
-}
+                  ) : (
+                    <></>
+                  )}
+                </td>
+              </tr>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </tbody>
+    </table>
+  );
+};
 
 export default DomainList;
--- src/components/trustedDomains.jsx
+++ src/components/trustedDomains.jsx
@@ -7,6 +7,8 @@ import { useState, useEffect } from "react";
 
 const TrustedDomains = () => {
   const [trustedDomains, setTrustedDomains] = useState(null);
+  const [domainToAdd, setDomainToAdd] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     getTrustedDomains(setTrustedDomains);
@@ -19,6 +21,48 @@ const TrustedDomains = () => {
         <h1>Trusted Domains</h1>
       </div>
       <SearchBar placeholder_value="Email domain name" />
+      {localStorage.getItem("sc_list_owner").toString() ===
+      localStorage.getItem("sc_email") ? (
+        <div className="add-item">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Domain name"
+              aria-label="Domain name"
+              onChange={(e) => {
+                setDomainToAdd(e.target.value);
+              }}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+              onClick={() => {
+                addTrustedDomain(
+                  domainToAdd,
+                  setErrorMessage,
+                  getTrustedDomains,
+                  setTrustedDomains
+                );
+              }}
+            >
+              Add
+            </button>
+          </div>
+          <div
+            id="errorMessage"
+            className={
+              "mb-4 text-" +
+              (errorMessage === "Successfully added" ? "success" : "danger")
+            }
+          >
+            {errorMessage}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
       {trustedDomains ? (
         <DomainList retrieved_list={trustedDomains} />
       ) : (
@@ -30,7 +74,7 @@ const TrustedDomains = () => {
   );
 };
 
-function getTrustedDomains(setTrustedDomains) {
+async function getTrustedDomains(setTrustedDomains) {
   var myHeaders = new Headers();
   myHeaders.append(
     "Authorization",
@@ -70,6 +114,70 @@ function getTrustedDomains(setTrustedDomains) {
       }
     })
     .catch((error) => console.log("error", error));
+}
+
+async function addTrustedDomain(
+  domainToAdd,
+  setErrorMessage,
+  getTrustedDomains,
+  setTrustedDomains
+) {
+  let add_button = document.getElementById("errorMessage");
+  add_button.disabled = true;
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "Bearer " + localStorage.getItem("sc_acc_token")
+  );
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    domain: domainToAdd,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  await fetch("http://localhost:4000/trusted_domains", requestOptions)
+    .then((response) => {
+      if (response.status === 500) {
+        console.clear();
+        return null;
+      } else if (response.status === 403) {
+        requestNewToken(addTrustedDomain, [
+          domainToAdd,
+          setErrorMessage,
+          getTrustedDomains,
+          setTrustedDomains,
+        ]);
+      } else if (response.ok) {
+        return response;
+      } else if (response.status === 406) {
+        return response.json();
+      }
+      return null;
+    })
+    .then((result) => {
+      if (result) {
+        if (result.details) {
+          setErrorMessage(
+            result.details[0].message.replace(/"domain"/, "Domain")
+          );
+        } else if (result.message) {
+          setErrorMessage(result.message);
+        } else {
+          setErrorMessage("Successfully added");
+          getTrustedDomains(setTrustedDomains);
+        }
+      }
+    })
+    .catch((error) => console.log("error", error));
+
+  add_button.disabled = false;
 }
 
 export default TrustedDomains;
