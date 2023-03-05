b36fee2bf9f0ffa93028b02ad51234a93b6665b5
Omar Majzoub <omarmajzoub1@hotmail.com>
2023-03-05 18:05:12 +0200

    Added logo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,15 +5,14 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div className="px-4 py-5 my-5 text-center">
-          <img
-            className="d-block mx-auto mb-4"
-            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
-            alt=""
-            width="72"
-            height="57"
-          />
-          <h1 className="display-5 fw-bold mb-4">SenderCheck</h1>
+        <img
+          className="d-block mx-auto"
+          src={require("../assets/SenderCheck-logos/SenderCheck-logos_black_crop.png")}
+          alt=""
+          width="40%"
+        />
+        <div className="px-4  text-center">
+          {/* <h1 className="fw-bold mb-4">SenderCheck</h1> */}
           <div className="col-lg-6 mx-auto">
             <p className="lead mb-4">
               SenderCheck helps you protect yourself from email fraudsters as
